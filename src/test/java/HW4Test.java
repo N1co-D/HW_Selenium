fc9e9742e646866aa06f88a2575715afa9a8b22a
dmark <dmarkevich@gmail.com>
2024-01-24 16:33:51 +0300

    [HW 4] Improved some tests

--- src/test/java/HW4Test.java
+++ src/test/java/HW4Test.java
@@ -8,44 +8,16 @@ import org.testng.annotations.Test;
 
 public class HW4Test {
     private WebDriver driver1;
-    private WebDriver driver2;
 
     @BeforeClass
     public void preconditions() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         driver1 = new ChromeDriver();
-        driver2 = new ChromeDriver();
     }
 
     @Test
     public void currentUrlTest() {
-        driver1.get("https://button-one.ru/");
-        driver2.get("https://store.steampowered.com/?l=russian");
-
-        String url1 = driver1.getCurrentUrl();
-        String url2 = driver2.getCurrentUrl();
-
-        Assert.assertEquals(url1, "https://button-one.ru/");
-        Assert.assertEquals(url2, "https://store.steampowered.com/?l=russian");
-    }
-
-    @Test
-    public void pageTitleTest() {
-        driver1.get("https://button-one.ru/");
-        driver2.get("https://store.steampowered.com/?l=russian");
-
-        String title1 = driver1.getTitle();
-        String title2 = driver2.getTitle();
-
-        Assert.assertEquals(title1, "Компания Button One");
-        Assert.assertEquals(title2, "Добро пожаловать в Steam");
-    }
-
-    @Test
-    public void navigateToTest() {
-        driver1.get("https://button-one.ru/");
-
-        driver1.navigate().to("https://store.steampowered.com/?l=russian");
+        driver1.get("https://store.steampowered.com/?l=russian");
 
         String url1 = driver1.getCurrentUrl();
 
@@ -53,11 +25,19 @@ public class HW4Test {
     }
 
     @Test
-    public void navigateBackTest() {
-        driver1.get("https://button-one.ru/");
+    public void pageTitleTest() {
+        driver1.get("https://store.steampowered.com/?l=russian");
 
-        driver1.navigate().to("https://store.steampowered.com/?l=russian");
-        driver1.navigate().back();
+        String title1 = driver1.getTitle();
+
+        Assert.assertEquals(title1, "Добро пожаловать в Steam");
+    }
+
+    @Test
+    public void navigateToTest() {
+        driver1.get("https://store.steampowered.com/?l=russian");
+
+        driver1.navigate().to("https://button-one.ru/");
 
         String url1 = driver1.getCurrentUrl();
 
@@ -65,25 +45,37 @@ public class HW4Test {
     }
 
     @Test
-    public void navigateForwardTest() {
-        driver1.get("https://button-one.ru/");
+    public void navigateBackTest() {
+        driver1.get("https://store.steampowered.com/?l=russian");
 
-        driver1.navigate().to("https://store.steampowered.com/?l=russian");
+        driver1.navigate().to("https://button-one.ru/");
+        driver1.navigate().back();
+
+        String url1 = driver1.getCurrentUrl();
+
+        Assert.assertEquals(url1, "https://store.steampowered.com/?l=russian");
+    }
+
+    @Test
+    public void navigateForwardTest() {
+        driver1.get("https://store.steampowered.com/?l=russian");
+
+        driver1.navigate().to("https://button-one.ru/");
         driver1.navigate().back();
         driver1.navigate().forward();
 
         String url1 = driver1.getCurrentUrl();
 
-        Assert.assertEquals(url1, "https://store.steampowered.com/?l=russian");
+        Assert.assertEquals(url1, "https://button-one.ru/");
     }
 
     @Test
     public void findElementTest() {
-        driver2.get("https://store.steampowered.com/?l=russian");
+        driver1.get("https://store.steampowered.com/?l=russian");
 
-        WebElement element2 = driver2.findElement(By.id("language_pulldown"));
+        WebElement element1 = driver1.findElement(By.id("language_pulldown"));
 
-        Assert.assertNotNull(element2);
+        Assert.assertNotNull(element1);
     }
 
 }
