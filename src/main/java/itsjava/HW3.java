0d170f301be02d8b59ef1334f6f7a7a16c6b03d9
dmark <dmarkevich@gmail.com>
2024-01-24 11:14:55 +0300

    [HW 3] Adding project files

---
+++ src/main/java/itsjava/HW3.java
@@ -0,0 +1,29 @@
+package itsjava;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class HW3 {
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("src/main/resources/config.properties");
+        Properties props = new Properties();
+        props.load(new FileInputStream(file));
+
+        if (props.getProperty("browser").equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://button-one.ru/");
+        } else {
+            System.err.println("Для работы требуется браузер Google Chrome");
+        }
+
+    }
+
+}
