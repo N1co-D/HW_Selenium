842ce749d84851c4e188e260e77ebae336f3935e
dmark <dmarkevich@gmail.com>
2024-01-25 18:42:50 +0300

    [HW 3] Improved

---
+++ src/main/java/firstlesson/HW3Improved.java
@@ -0,0 +1,45 @@
+package firstlesson;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class HW3Improved {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("src/main/resources/config.properties");
+        Properties props = new Properties();
+        props.load(new FileInputStream(file));
+        WebDriver driver = null;
+
+        if (props.getProperty("browser").equals("chrome")) {
+            System.out.println("Определен браузер: Google Chrome");
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+//            driver = WebDriverManager.chromedriver().create(); // Логика также работает без двух предыдущих строк
+        } else if (props.getProperty("browser").equals("firefox")) {
+            System.out.println("Определен браузер: Mozilla Firefox");
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+//            driver = WebDriverManager.firefoxdriver().create(); // Логика также работает без двух предыдущих строк
+        } else if (props.getProperty("browser").equals("edge")) {
+            System.out.println("Определен браузер: Microsoft Edge");
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+//            driver = WebDriverManager.edgedriver().create(); // Логика также работает без двух предыдущих строк
+        } else {
+            System.out.println("Браузер не определен");
+        }
+
+        driver.get("https://button-one.ru/");
+        driver.quit();
+
+    }
+}
