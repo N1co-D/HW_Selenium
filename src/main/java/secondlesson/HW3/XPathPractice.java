192964d19753069646aebdf8acc758bec34617a0
dmark <dmarkevich@gmail.com>
2024-01-26 17:23:42 +0300

    [HW 3] Renamed

--- src/main/java/secondlesson/HW3/XPathPractice.java
+++ src/main/java/secondlesson/HW3/XPathPractice.java
@@ -21,38 +21,38 @@ public class XPathPractice {
 
         driver.get("https://store.steampowered.com/?l=russian");
 
-        WebElement element1 = driver.findElement(By.xpath("//h2[@class = 'home_page_content_title' and text() = 'Популярное и рекомендуемое']")); // Раздел "Популярное и рекомендуемое" под основным баннером
-        WebElement element2 = driver.findElement(By.xpath("//a[@href = 'https://store.steampowered.com/category/pirates_ninjas?snr=1_4_4__promo-takeunder' and @class = 'home_page_takeunder']")); // Рекламный баннер с фестивалем пиратов
-        WebElement element3 = driver.findElement(By.xpath("//div[@class = 'home_ctn' and id = 'module_special_offers'] | //div[@class = 'carousel_thumbs' and @data-usability = '2']/div[@class = 'focus']")); // Первый нижний переключатель под баннером "Специальные предложения"
-        WebElement element4 = driver.findElement(By.xpath("//div[@class = 'home_ctn']//div[@class = 'home_page_sign_in_ctn small']/div[@class='signin_buttons_ctn']/a[text()='присоединитесь']")); // Кнопка "присоединитесь" под разделом-баннером "Категории"
-        WebElement element5 = driver.findElement(By.xpath("//a[@href = 'https://store.steampowered.com/explore/new/?snr=1_4_4__tabsmore']/span[text() = 'Новинки']")); // Кнопка "Новинки" в списки игр с разделом "Популярные новинки
+        WebElement popularAndRecommendedSectionUnderMainBanner = driver.findElement(By.xpath("//h2[@class = 'home_page_content_title' and text() = 'Популярное и рекомендуемое']")); // Раздел "Популярное и рекомендуемое" под основным баннером
+        WebElement advertisingBannerBetweenSections = driver.findElement(By.xpath("//a[@href = 'https://store.steampowered.com/category/pirates_ninjas?snr=1_4_4__promo-takeunder' and @class = 'home_page_takeunder']")); // Рекламный баннер между разделами
+        WebElement firstLowerSwitchUnderTheSpecialOffersBanner = driver.findElement(By.xpath("//div[@class = 'home_ctn' and id = 'module_special_offers'] | //div[@class = 'carousel_thumbs' and @data-usability = '2']/div[@class = 'focus']")); // Первый нижний переключатель под баннером "Специальные предложения"
+        WebElement joinButtonUnderTheCategoriesBanner = driver.findElement(By.xpath("//div[@class = 'home_ctn']//div[@class = 'home_page_sign_in_ctn small']/div[@class='signin_buttons_ctn']/a[text()='присоединитесь']")); // Кнопка "присоединитесь" под разделом-баннером "Категории"
+        WebElement newItemsButtonInTheGameListsWithThePopularNewItemsSection = driver.findElement(By.xpath("//a[@href = 'https://store.steampowered.com/explore/new/?snr=1_4_4__tabsmore']/span[text() = 'Новинки']")); // Кнопка "Новинки" в списки игр с разделом "Популярные новинки"
 
-        System.out.println("element1.getText() = " + element1.getText());
-        System.out.println("element2.isDisplayed() = " + element2.isDisplayed());
-        System.out.println("element3.isSelected() = " + element3.isSelected());
-        System.out.println("element4.isEnabled() = " + element4.isEnabled());
-        System.out.println("element5.getLocation() = " + element5.getLocation());
+        System.out.println("popularAndRecommendedSectionUnderMainBanner.getText() = " + popularAndRecommendedSectionUnderMainBanner.getText());
+        System.out.println("advertisingBannerBetweenSections.isDisplayed() = " + advertisingBannerBetweenSections.isDisplayed());
+        System.out.println("firstLowerSwitchUnderTheSpecialOffersBanner.isSelected() = " + firstLowerSwitchUnderTheSpecialOffersBanner.isSelected());
+        System.out.println("joinButtonUnderTheCategoriesBanner.isEnabled() = " + joinButtonUnderTheCategoriesBanner.isEnabled());
+        System.out.println("newItemsButtonInTheGameListsWithThePopularNewItemsSection.getLocation() = " + newItemsButtonInTheGameListsWithThePopularNewItemsSection.getLocation());
 
         System.out.println();
 
-        WebElement element6 = driver.findElement(By.xpath("//h2[@class = 'home_page_content_title' and text() = 'Специальные предложения\t\t\t\t\t\t\t\t\t']//ancestor::div[@id= 'module_special_offers']")); // Модуль "Специальные предложения"
-        WebElement element7 = driver.findElement(By.xpath("//div[@class = 'home_ctn']//descendant::a[@class = 'btn_green_white_innerfade btn_border_2px btn_medium']")); // Модуль "Специальные предложения"
-        WebElement element8 = driver.findElement(By.xpath("//div[@class = 'valve_links']//following::a[@target = '_blank']")); // Все кнопки в нижней строки страницы
-        WebElement element9 = driver.findElement(By.xpath("//div[@class = 'gutter_header pad' and text() = 'Поиск по жанру']//parent::div[@class = 'home_page_gutter_block']")); // Модуль "Поиск по жанру" в боковом меню
-        WebElement element10 = driver.findElement(By.xpath("//div[@class = 'searchbox']/child::input")); // Поле для поиска
-        WebElement element11 = driver.findElement(By.xpath("//div[@class = 'logo']//child::img")); // Картинка логотипа в шапке сайта
+        WebElement specialOffersModuleWithAncestor = driver.findElement(By.xpath("//h2[@class = 'home_page_content_title' and text() = 'Специальные предложения\t\t\t\t\t\t\t\t\t']//ancestor::div[@id= 'module_special_offers']")); // Модуль "Специальные предложения"
+        WebElement specialOffersModuleWithDescendant = driver.findElement(By.xpath("//div[@class = 'home_ctn']//descendant::a[@class = 'btn_green_white_innerfade btn_border_2px btn_medium']")); // Модуль "Специальные предложения"
+        WebElement allButtonsBottomMenu = driver.findElement(By.xpath("//div[@class = 'valve_links']//following::a[@target = '_blank']")); // Все кнопки в нижней строки страницы
+        WebElement genreSearchModuleSideMenu = driver.findElement(By.xpath("//div[@class = 'gutter_header pad' and text() = 'Поиск по жанру']//parent::div[@class = 'home_page_gutter_block']")); // Модуль "Поиск по жанру" в боковом меню
+        WebElement searchField = driver.findElement(By.xpath("//div[@class = 'searchbox']/child::input")); // Поле для поиска
+        WebElement headerLogoImage = driver.findElement(By.xpath("//div[@class = 'logo']//child::img")); // Картинка логотипа в шапке сайта
 
-//        List<WebElement> elements = driver.findElements(By.xpath("//div[@id = 'home_maincap_v7']//div[@class = 'carousel_thumbs']/div//attribute::class")); // Все переключатели под баннером "Популярное и рекомендуемое"
+//        List<WebElement> allSwitchesUnderPopularAndRecommendedBanner = driver.findElements(By.xpath("//div[@id = 'home_maincap_v7']//div[@class = 'carousel_thumbs']/div//attribute::class")); // Все переключатели под баннером "Популярное и рекомендуемое"
 
-        System.out.println("element6.isDisplayed() = " + element6.isDisplayed());
-        System.out.println("element7.getSize() = " + element7.getSize());
-        System.out.println("element8.isEnabled() = " + element8.isEnabled());
-        System.out.println("element9.getCssValue(\"width\") = " + element9.getCssValue("width"));
-        element10.clear();
-        System.out.println("element11.getAttribute(\"src\") = " + element11.getAttribute("src"));
+        System.out.println("specialOffersModuleWithAncestor.isDisplayed() = " + specialOffersModuleWithAncestor.isDisplayed());
+        System.out.println("specialOffersModuleWithDescendant.getSize() = " + specialOffersModuleWithDescendant.getSize());
+        System.out.println("allButtonsBottomMenu.isEnabled() = " + allButtonsBottomMenu.isEnabled());
+        System.out.println("genreSearchModuleSideMenu.getCssValue(\"width\") = " + genreSearchModuleSideMenu.getCssValue("width"));
+        searchField.clear();
+        System.out.println("headerLogoImage.getAttribute(\"src\") = " + headerLogoImage.getAttribute("src"));
 
-//        for (int i = 0; i < elements.size(); i++) {
-//            System.out.print(elements.get(i).getCssValue("color") + " ");
+//        for (int i = 0; i < allSwitchesUnderPopularAndRecommendedBanner.size(); i++) {
+//            System.out.print(allSwitchesUnderPopularAndRecommendedBanner.get(i).getCssValue("color") + " ");
 //        }
 
         driver.quit();
