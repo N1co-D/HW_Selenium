4e183747ec90372664e426e19c955461d8e489aa
dmark <dmarkevich@gmail.com>
2024-01-30 11:10:46 +0300

    [HW 1] Correct mistakes

--- src/test/java/secondlesson/HW1/FindingElementPracticeTest.java
+++ src/test/java/secondlesson/HW1/FindingElementPracticeTest.java
@@ -12,11 +12,10 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.io.*;
+import java.time.Duration;
 import java.util.*;
 
 public class FindingElementPracticeTest {
@@ -35,10 +34,16 @@ public class FindingElementPracticeTest {
     }
 
     @BeforeClass
-    public WebDriver browserDefinition() throws IOException {
+    public WebDriver browserDefinition() {
         File file = new File("src/main/resources/config.properties");
         Properties props = new Properties();
-        props.load(new FileInputStream(file));
+
+        try {
+            props.load(new FileInputStream(file));
+        } catch (IOException e) {
+            System.err.println("Ошибка при загрузке файла!");
+            throw new RuntimeException(e);
+        }
 
         if (props.getProperty("browser").equals("chrome")) {
             System.out.println("Определен браузер: Google Chrome");
@@ -65,42 +70,42 @@ public class FindingElementPracticeTest {
         return driver;
     }
 
-    @Test
-    public void categoriesButtonTopMenuDesktopDisplayingTest() throws IOException {
-        WebDriver driver = browserDefinition();
+    @Test (priority = 1)
+    public void categoriesButtonTopMenuDesktopDisplayingTest() {
         driver.get("https://store.steampowered.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
         WebElement categoriesButtonTopMenuDesktop = driver.findElement(By.xpath("//span//a[@class = 'pulldown_desktop' and text() = 'Категории']"));
-        Assert.assertTrue(categoriesButtonTopMenuDesktop.isDisplayed());
-        driver.quit();
+        Assert.assertTrue(categoriesButtonTopMenuDesktop.isDisplayed(), "[Десктопная версия] Кнопка \"Категории\" в верхнем меню страницы не отображается.");
     }
 
-    @Test(dataProvider = "devicesData")
-    public void categoriesButtonTopMenuMobileDisplayingTest(String deviceName, int width, int height) throws IOException {
-        WebDriver driver = browserDefinition();
+    @Test (priority = 2)
+    public void categorySectionSideMenuDisplayingTest() {
+        driver.get("https://store.steampowered.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+        WebElement categoriesButtonTopMenuDesktop = driver.findElement(By.xpath("//div[@class = 'gutter_header pad' and text() = 'Категории']"));
+        Assert.assertTrue(categoriesButtonTopMenuDesktop.isDisplayed(), "Заголовок раздела \"Категории\" в боковом меню страницы не отображается.");
+    }
+
+    @Test (priority = 3)
+    public void categorySectionCenterPageDisplayingTest() {
+        driver.get("https://store.steampowered.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+        WebElement categoriesButtonTopMenuDesktop = driver.findElement(By.xpath("//div[@class = 'title' and text() = 'Категории']"));
+        Assert.assertTrue(categoriesButtonTopMenuDesktop.isDisplayed(), "Заголовок раздела \"Категории\" в центре страницы не отображается.");
+    }
+
+    @Test(priority = 4, dataProvider = "devicesData")
+    public void categoriesButtonTopMenuMobileDisplayingTest(String deviceName, int width, int height) {
         driver.manage().window().setSize(new Dimension(width, height));
         driver.get("https://store.steampowered.com/");
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
         WebElement categoriesButtonTopMenuMobile = driver.findElement(By.xpath("//span//a[@class = 'pulldown_mobile' and text() = 'Категории']"));
-        Assert.assertTrue(categoriesButtonTopMenuMobile.isDisplayed());
+        Assert.assertTrue(categoriesButtonTopMenuMobile.isDisplayed(), "[Мобильная версия] Кнопка \"Категории\" в верхнем меню страницы не отображается.");
+    }
 
+    @AfterTest
+    public void afterTests() {
         driver.quit();
     }
 
-    @Test
-    public void categorySectionSideMenuDisplayingTest() throws IOException {
-        WebDriver driver = browserDefinition();
-        driver.get("https://store.steampowered.com/");
-        WebElement categoriesButtonTopMenuDesktop = driver.findElement(By.xpath("//div[@class = 'gutter_header pad' and text() = 'Категории']"));
-        Assert.assertTrue(categoriesButtonTopMenuDesktop.isDisplayed());
-        driver.quit();
-    }
-
-    @Test
-    public void categorySectionCenterPageDisplayingTest() throws IOException {
-        WebDriver driver = browserDefinition();
-        driver.get("https://store.steampowered.com/");
-        WebElement categoriesButtonTopMenuDesktop = driver.findElement(By.xpath("//div[@class = 'title' and text() = 'Категории']"));
-        Assert.assertTrue(categoriesButtonTopMenuDesktop.isDisplayed());
-        driver.quit();
-    }
 }
